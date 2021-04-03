832597255c2e0a7f1d8356b4b938a87db3c567f9
Divyesh <divyeshrupawala@gmail.com>
2021-04-03 17:46:56 +0530

    Added routing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LearnAngularComponent } from './learn-angular/learn-angular.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  { path: '', component: HomeComponent },
+  { path: 'learn-angular', component: LearnAngularComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
